cbb8bd254d90c2b4d20de6ba87e967c49f904739
didihansya <didihansya@didihansyas-MacBook-Air.local>
2020-05-16 19:17:26 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
 import 'screens/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -14,6 +12,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
